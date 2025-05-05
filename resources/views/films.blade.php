ed21880fca835d642ac91dc8ec29f7da5f71cf0f
Akhtar Hafiz P <akhfizpuli@gmail.com>
2025-05-05 13:40:14 +0700

    added films view

---
+++ resources/views/films.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Manajemen Film</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+    @include('navbar')
+
+    <div class="container mt-5">
+        <h1 class="mb-4">Manajemen Film</h1>
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
