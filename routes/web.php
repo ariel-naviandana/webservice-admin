05e9a9b10d192eade70c5b0bf33deeabecf4ce08
Ariel2711 <arielnaviandanaputra@gmail.com>
2025-05-05 12:33:29 +0700

    refactor route

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CastController;
 use App\Http\Controllers\GenreController;
+use App\Http\Controllers\UsersManagementController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users', [UsersManagementController::class, 'index'])->name('users.index');
+Route::put('/users/{id}', [UsersManagementController::class, 'update'])->name('users.update');
+Route::delete('/users/{id}', [UsersManagementController::class, 'destroy'])->name('users.destroy');
 
 Route::get('/genre', function () {
     return view('genre');
