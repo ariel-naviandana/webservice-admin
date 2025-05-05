d60c73ce990c64f742ae3b88349ec3ed7ba6f84d
stargazer <stargazer>
2025-05-05 11:03:31 +0700

    Update pop up tambah genre & cast

--- resources/views/cast.blade.php
+++ resources/views/cast.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
 
     <!-- Custom CSS -->
     <style>
@@ -35,7 +35,7 @@
 <body>
 <h2>Manajemen Cast</h2>
 <div class="table-container">
-    <button class="btn btn-success btn-sm">Tambah</button>
+    <button class="btn btn-success btn-sm" data-bs-toggle="modal" data-bs-target="#castModal">Tambah</button>
     <table class="table table-striped table-bordered">
         <thead>
         <tr>
@@ -65,8 +65,49 @@
     </table>
 </div>
 </div>
+
+<!-- Pop up gitulah -->
+<div class="modal fade" id="castModal" tabindex="-1" aria-labelledby="castModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <form action="/cast/store" method="POST">
+
+                @csrf
+                <div class="modal-header">
+                    <h5 class="modal-title" id="castModalLabel">Tambah Cast</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Nama</label>
+                        <input type="text" class="form-control" id="name" name="name" required>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="dob" class="form-label">Tanggal Lahir</label>
+                        <input type="date" class="form-control" id="dob" name="date_of_birth" required>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="img_url" class="form-label">URL Gambar</label>
+                        <input type="file" class="form-control" id="img_url" name="img_url" accept="image/*">
+                    </div>
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                    <button type="submit" class="btn btn-primary">Simpan</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
 </body>
 </html>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
 
 <?php
 
--- resources/views/genre.blade.php
+++ resources/views/genre.blade.php
@@ -31,7 +31,7 @@
 <body>
 <h2>Manajemen Genre</h2>
 <div class="table-container">
-    <button class="btn btn-success btn-sm">Tambah</button>
+    <button class="btn btn-success btn-sm" data-bs-toggle="modal" data-bs-target="#genreModal">Tambah</button>
     <table class="table table-striped table-bordered">
         <thead>
         <tr>
@@ -57,5 +57,34 @@
     </table>
 </div>
 </div>
+
+<div class="modal fade" id="genreModal" tabindex="-1" aria-labelledby="genreModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <form action="/cast/store" method="POST">
+
+                @csrf
+                <div class="modal-header">
+                    <h5 class="modal-title" id="castModalLabel">Tambah Genre</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Nama</label>
+                        <input type="text" class="form-control" id="name" name="name" required>
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 </body>
 </html>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
