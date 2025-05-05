8aa05a8ffe61184a457189d816cda2b5c1962200
Akhtar Hafiz P <akhfizpuli@gmail.com>
2025-05-05 13:40:43 +0700

    added route

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsersManagementController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [HomeController::class, 'index'])->name('home.index');
 
 Route::get('/users', [UsersManagementController::class, 'index'])->name('users.index');
 Route::put('/users/{id}', [UsersManagementController::class, 'update'])->name('users.update');
