caa00338b84a5bf2bc025dedc5d83afe9e8ad04a
Akhtar Hafiz P <akhfizpuli@gmail.com>
2025-05-10 17:32:53 +0700

    added routing to film view

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -8,7 +8,7 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item"><a class="nav-link" href="/home">Home</a></li>
-                <li class="nav-item"><a class="nav-link disabled" href="#">Manajemen Film</a></li>
+                <li class="nav-item"><a class="nav-link" href="/films">Manajemen Film</a></li>
                 <li class="nav-item"><a class="nav-link disabled" href="#">Manajemen Cast</a></li>
                 <li class="nav-item"><a class="nav-link disabled" href="#">Manajemen Genre</a></li>
                 <li class="nav-item"><a class="nav-link" href="/users">Manajemen User</a></li>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FilmManagementController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsersManagementController;
@@ -13,3 +14,5 @@ Route::get('/home', [HomeController::class, 'index'])->name('home.index');
 Route::get('/users', [UsersManagementController::class, 'index'])->name('users.index');
 Route::put('/users/{id}', [UsersManagementController::class, 'update'])->name('users.update');
 Route::delete('/users/{id}', [UsersManagementController::class, 'destroy'])->name('users.destroy');
+
+Route::get('/films', [FilmManagementController::class, 'index'])->name('films.index');
