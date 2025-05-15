af0a0d4036371904b6c0c9efe856ab05a87fe953
Akhtar Hafiz P <akhfizpuli@gmail.com>
2025-05-15 13:06:21 +0700

    added edit film

--- app/Http/Controllers/FilmManagementController.php
+++ app/Http/Controllers/FilmManagementController.php
@@ -21,10 +21,37 @@ class FilmManagementController extends Controller
     }
     public function update(Request $request, $id)
     {
-        //
+        // Validasi Input
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+            'synopsis' => 'nullable|string',
+            'release_year' => 'required|integer|min:1900|max:' . date('Y'),
+            'duration' => 'required|integer|min:1',
+            'poster_url' => 'nullable|url',
+            'director' => 'nullable|string|max:255'
+        ]);
+
+        $response = Http::put("{$this->apiBaseUrl}/{$id}", $validated);
+
+        if ($response->successful()) {
+            return redirect()->route('films.index')->with('success', 'Film berhasil diperbarui');
+        } else {
+            return back()->with('error', 'Gagal merubah film');
+        }
     }
     public function destroy()
     {
         //
+    }
+    public function edit($id)
+    {
+        $response = Http::get("{$this->apiBaseUrl}/{$id}");
+
+        if ($response->successful()) {
+            $film = $response->json();
+            return view('edit_films', compact('film'));
+        } else {
+            return redirect()->route('films.index')->with('error', 'Gagal mengambil data film.');
+        }
     }
 }
