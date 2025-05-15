0ae04eda3136033aa14b0e14de934a4135dfedb9
Akhtar Hafiz P <akhfizpuli@gmail.com>
2025-05-15 13:10:31 +0700

    added routes to view

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,7 +1,7 @@
 <!-- resources/views/navbar.blade.php -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Admin Panel</a>
+        <a class="navbar-brand" href="/home">Admin Panel</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -9,8 +9,8 @@
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item"><a class="nav-link" href="/home">Home</a></li>
                 <li class="nav-item"><a class="nav-link" href="/films">Manajemen Film</a></li>
-                <li class="nav-item"><a class="nav-link disabled" href="#">Manajemen Cast</a></li>
-                <li class="nav-item"><a class="nav-link disabled" href="#">Manajemen Genre</a></li>
+                <li class="nav-item"><a class="nav-link" href="/cast">Manajemen Cast</a></li>
+                <li class="nav-item"><a class="nav-link" href="/genre">Manajemen Genre</a></li>
                 <li class="nav-item"><a class="nav-link" href="/users">Manajemen User</a></li>
                 <li class="nav-item"><a class="nav-link disabled" href="#">Manajemen Review</a></li>
                 <li class="nav-item"><a class="nav-link disabled" href="#">Login/Logout</a></li>
