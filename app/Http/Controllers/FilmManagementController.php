356adf6c48d1a5f142f271efd6617c3490703e82
Akhtar Hafiz P <akhfizpuli@gmail.com>
2025-05-15 16:38:55 +0700

    updated CRUD functions

--- app/Http/Controllers/FilmManagementController.php
+++ app/Http/Controllers/FilmManagementController.php
@@ -8,44 +8,46 @@ use Illuminate\Support\Facades\Http;
 class FilmManagementController extends Controller
 {
 
-    private $apiBaseUrl = 'http://localhost:8000/api/films';
+    private $apiBaseUrl = 'http://localhost:8000/api';
     public function index()
     {
-        $response = Http::get("{$this->apiBaseUrl}");
+        $response = Http::get("{$this->apiBaseUrl}/films");
         if ($response->successful()) {
             $films = $response->json();
             return view('films', compact('films'));
         } else {
-            return view('users');
+            return view('films');
         }
+    }
+    public function store()
+    {
+        $response = Http::post("{$this->apiBaseUrl}/films");
     }
     public function update(Request $request, $id)
     {
         // Validasi Input
-        $validated = $request->validate([
-            'title' => 'required|string|max:255',
-            'synopsis' => 'nullable|string',
-            'release_year' => 'required|integer|min:1900|max:' . date('Y'),
-            'duration' => 'required|integer|min:1',
-            'poster_url' => 'nullable|url',
-            'director' => 'nullable|string|max:255'
-        ]);
-
-        $response = Http::put("{$this->apiBaseUrl}/{$id}", $validated);
+        $response = Http::put("{$this->apiBaseUrl}/films/{$id}", $request);
 
         if ($response->successful()) {
-            return redirect()->route('films.index')->with('success', 'Film berhasil diperbarui');
+            return redirect()->route('films.index')->with('success', 'Film berhasil diperbarui.');
         } else {
             return back()->with('error', 'Gagal merubah film');
         }
     }
-    public function destroy()
+    public function destroy($id)
     {
-        //
+        $response = Http::delete("{$this->apiBaseUrl}/films/{$id}");
+        if ($response->successful()) {
+            return redirect()->route('films.index')->with('message', 'Film berhasil dihapus.');
+        } else {
+            return back()->with('error', 'Gagal menghapus film dengan ID tersebut');
+        }
     }
     public function edit($id)
     {
-        $response = Http::get("{$this->apiBaseUrl}/{$id}");
+        $response = Http::get("{$this->apiBaseUrl}/films/{$id}");
+        // $responseCast = Http::get("{$this->apiBaseUrl}/{$id}");
+        // $responseGenres = Http::get("{$this->apiBaseUrl}/{$id}");
 
         if ($response->successful()) {
             $film = $response->json();
@@ -53,5 +55,12 @@ class FilmManagementController extends Controller
         } else {
             return redirect()->route('films.index')->with('error', 'Gagal mengambil data film.');
         }
+    }
+    public function add()
+    {
+        // $response = Http::get("{$this->apiBaseUrl}/films");
+        // $responseCast = Http::get("{$this->apiBaseUrl}/");
+        // $responseGenres = Http::get("{$this->apiBaseUrl}/{$id}");
+        return view('add_films');
     }
 }
